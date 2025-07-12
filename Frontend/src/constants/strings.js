25363fd80604969f4eede3292b0203dc3c1336a8
Kshitiz <kshitizyadavofficial@gmail.com>
2025-07-13 01:32:47 +0530

    feat: added signin form, site logos, dummy home page

---
+++ Frontend/src/constants/strings.js
@@ -0,0 +1,12 @@
+const STRINGS = {
+  signIn: {
+    signIn: "Sign In",
+    signUp: "Sign Up",
+    emailLabel: "Email Address",
+    passwordLabel: "Password",
+    noAccount: "Don't have an account? ",
+    existingUser: "Already have an account? "
+  }
+};
+
+export default STRINGS;
