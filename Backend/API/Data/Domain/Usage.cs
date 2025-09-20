a2a5e31942a98c6e045f31bcb66fdc641b0ab23b
Kshitiz <kshitizyadavofficial@gmail.com>
2025-09-21 00:01:44 +0530

    fix: added output column

--- Backend/API/Data/Domain/Usage.cs
+++ Backend/API/Data/Domain/Usage.cs
@@ -7,5 +7,6 @@ public class Usage
     public int UserId { get; set; }
     public required string Month { get; set; }
     public Resource Resource { get; set; }
-    public long Used { get; set; }
+    public long InputUsed { get; set; }
+    public long OutputUsed { get; set; }
 }
---
+++ Backend/API/Data/Migrations/20250920181907_AddedOutputLimit.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedOutputLimit : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Used",
+                table: "UserLimits",
+                newName: "OutputUsed");
+
+            migrationBuilder.AddColumn<long>(
+                name: "InputUsed",
+                table: "UserLimits",
+                type: "bigint",
+                nullable: false,
+                defaultValue: 0L);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "InputUsed",
+                table: "UserLimits");
+
+            migrationBuilder.RenameColumn(
+                name: "OutputUsed",
+                table: "UserLimits",
+                newName: "Used");
+        }
+    }
+}
