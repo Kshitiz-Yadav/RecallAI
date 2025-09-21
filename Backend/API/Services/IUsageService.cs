49eb176c89d71fd049166b2771aac748010d3201
Kshitiz <kshitizyadavofficial@gmail.com>
2025-09-21 13:05:08 +0530

    feat: added usage service

---
+++ Backend/API/Services/IUsageService.cs
@@ -0,0 +1,12 @@
+﻿using API.Enums;
+
+namespace API.Services;
+
+public interface IUsageService
+{
+    public Task<int> CheckResourceUsage(Resource resource, int userId, long expectedInput);
+
+    public Task UpdateResourceUsage(Resource resource, int userId, long inputChange, long outputChange = 0);
+
+    public int GetExpectedTokensCount(string text);
+}
