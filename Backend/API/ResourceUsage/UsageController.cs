9d27376ce452be5d450f837596b79a7fb27b1768
Kshitiz <kshitizyadavofficial@gmail.com>
2025-09-21 17:34:57 +0530

    feat: added usage controller

---
+++ Backend/API/ResourceUsage/UsageController.cs
@@ -0,0 +1,51 @@
+﻿using API.Data;
+using API.Dto.Resources;
+using API.Enums;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using static API.Common;
+
+namespace API.ResourceUsage;
+
+[Authorize]
+[ApiController]
+[Route("api/[controller]")]
+public class UsageController : Controller
+{
+    private readonly AppSettings _appSettings;
+    private readonly DatabaseContext _dbContext;
+    private readonly ILogger<UsageController> _logger;
+    private readonly string CurrentMonth = $"{DateTime.UtcNow:MM}{DateTime.UtcNow:yyyy}";
+
+    public UsageController(AppSettings appSettings, DatabaseContext dbContext, ILogger<UsageController> logger)
+    {
+        _appSettings = appSettings;
+        _logger = logger;
+        _dbContext = dbContext;
+    }
+
+    [HttpGet("limits")]
+    public IActionResult GetResourceLimits()
+    {
+        _logger.LogInformation("Get resource limits request received.");
+        return new OkObjectResult(_appSettings.ResourceLimits);
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetCurrentMonthUsage()
+    {
+        _logger.LogInformation("Get monthly usage request received.");
+        var userId = GetUserId(User);
+
+        var usages = await _dbContext.UserLimits
+            .Where(x => x.UserId == userId && (x.Month == CurrentMonth || x.Resource == Resource.FileStorage))
+            .ToDictionaryAsync(u => u.Resource, u => new ResourceLimits
+            {
+                Input = u.InputUsed,
+                Output = u.OutputUsed
+            });
+
+        return new OkObjectResult(usages);
+    }
+}
